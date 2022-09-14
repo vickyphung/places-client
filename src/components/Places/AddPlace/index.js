33e733d6fca9850e21c215671efc120cea71b5e0
vicky phung <vickyphung@gmail.com>
2022-09-14 16:22:18 -0400

    removed zip from addfrommaps form

--- src/components/Places/AddPlace/index.js
+++ src/components/Places/AddPlace/index.js
@@ -36,7 +36,7 @@ const AddPlace = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await axios.post("https://placeswithbear.herokuapp.com/places/add", {
+    const response = await axios.post("http://localhost:8800/places/add", {
       name: placeFormData.placeName,
       location: {
         street: placeFormData.placeStreet,
